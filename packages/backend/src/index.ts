403e8c512f423fab5410e42b1b34d7fb3a9fa1dd
Ajuvendra Kumar <ajuvendra.kumar@kcc.com>
2024-06-01 09:04:16 +0530

    adding github authentication

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -8,6 +8,7 @@
 
 import { createBackend } from '@backstage/backend-defaults';
 
+
 const backend = createBackend();
 
 backend.add(import('@backstage/plugin-app-backend/alpha'));
@@ -16,7 +17,9 @@ backend.add(import('@backstage/plugin-scaffolder-backend/alpha'));
 backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
 
 // auth plugin
+
 backend.add(import('@backstage/plugin-auth-backend'));
+backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
 // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 // See https://backstage.io/docs/auth/guest/provider
